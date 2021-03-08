5f4ca6403069a9028a4cbf54860857f6b30776bf
= <zachlweaver00@gmail.com>
2021-03-07 21:15:30 -0500

    version 0.2.0 package

---
+++ data/DataSet.h
@@ -0,0 +1,341 @@
+#ifndef DATASET_H
+#define DATASET_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <fstream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+// only supports delimiters of one character length
+class DataSet {
+    private:
+        bool has_headers = true;
+
+        // load rows into data matrix
+        void split(string text, int current_row, char sep = ',') {
+            bool inside_string = false;
+            int start_index = 0;
+
+            for (int i = 0; i < text.length(); i++) {
+                if (text[i] == sep && !inside_string) {
+                    data[current_row].push_back(text.substr(start_index, i - start_index));
+                    start_index = i + 1;
+                }
+
+                if (i == text.length() - 1) {
+                    data[current_row].push_back(text.substr(start_index, text.length()));
+                }
+
+                // check if we're inside quotes
+                if (text[i] == '\"' && !inside_string) { inside_string = true; }
+                else if (text[i] == '\"' && inside_string) { inside_string = false; }
+            }
+
+        }
+
+        // load headers (if exists) into columns vector
+        void split(string text, char sep = ',') {
+            bool inside_string = false;
+            int start_index = 0;
+
+            for (int i = 0; i < text.length(); i++) {
+                if (text[i] == sep && !inside_string) {
+                    columns.push_back(text.substr(start_index, i - start_index));
+                    start_index = i + 1;
+                }
+
+                if (i == text.length() - 1) {
+                    columns.push_back(text.substr(start_index, text.length()));
+                }
+
+                // check if we're inside quotes
+                if (text[i] == '\"' && !inside_string) { inside_string = true; }
+                else if (text[i] == '\"' && inside_string) { inside_string = false; }
+            }
+        }
+    public:
+        vector<vector<string>> data;
+        int data_size;
+        vector<string> columns;
+
+        // load from file
+        void load(string filepath, char sep = ',', bool has_headers = true) {
+            this->has_headers = has_headers;
+
+            string current_line;
+            int current_row = 0;
+
+            try {
+                ifstream datafile(filepath);
+
+                // check if file exists
+                if (datafile.fail()) {
+                    cout << "There was a problem loading your data!" << endl;
+                    cout << "Check your directory/filename." << endl;
+                }
+
+                // read data
+                if (datafile.is_open()) {
+                    while (getline(datafile, current_line)) {
+                        data.push_back(vector<string>()); // create rows
+                        if (has_headers) {
+                            // populate columns vector (if headers exist)
+                            split(current_line, sep);
+                            has_headers = false;
+                        }
+                        else {
+                            // populate data matrix
+                            split(current_line, current_row, sep);
+                            current_row += 1;
+                        }
+                    }
+                }
+                datafile.close();
+                // BUG: data.size() is messed up when reading from CSV. Need to look into
+                // This is temporary fix
+                this->data_size = data.size() - 1;
+            }
+            catch(int e) { }
+        }
+
+        // load from another data set (with columns)
+        void load(vector<vector<string>> data, vector<string> columns) {
+            this->data = data;
+            this->columns = columns;
+        }
+
+        void head(int rows = 10) {
+            // if column vector exists, print columns before data
+            if (columns.size() > 0) {
+                for (int c = 0; c < columns.size(); ++c) {
+                    cout << columns[c] << "\t\t";
+                }
+            }
+            cout << endl;
+
+
+            // print #[rows] of data
+            for (int r = 0; r < rows; ++r) {
+                if (r >= this->data_size) { break; } // check if rows is larger than our data set
+                else {
+                    for (int c = 0; c < data[0].size(); ++c) {
+                        cout << data[r][c] << "\t\t";
+                    }
+                    cout << endl;
+                }
+            }
+        }
+
+        // select subset of original data set by index (creates new data set)
+        DataSet select(vector<int> indices) {
+            int new_size = indices.size();
+
+            vector<string> new_columns;
+
+            if (has_headers) {
+                for (int i = 0; i < new_size; ++i) {
+                    new_columns.push_back(columns[indices[i]]);
+                }
+            }
+
+            vector<vector<string>> new_data;
+
+            for (int r = 0; r < data.size() - 1; ++r) {
+                new_data.push_back(vector<string>());
+                for (int c = 0; c < new_size; ++c) {
+                    new_data[r].push_back(data[r][indices[c]]);
+                }
+            }
+
+            DataSet subset;
+            subset.load(new_data, new_columns);
+            subset.data_size = new_data.size();
+
+            return subset;
+        }
+
+        // select subset of original data set by name (creates new data set)
+        DataSet select(vector<string> names) {
+            int new_size = names.size();
+
+            // create map of indices from original column names to positions
+            map<string, int> name_index;
+            for (int i = 0; i < columns.size(); ++i) {
+                name_index.insert(pair<string,int>(columns[i], i));
+            }
+
+            vector<string> new_columns;
+            for (int i = 0; i < new_size; ++i) {
+                new_columns.push_back(names[i]);
+            }
+
+            vector<vector<string>> new_data;
+
+            for (int r = 0; r < data.size() - 1; ++r) {
+                new_data.push_back(vector<string>());
+                for (int c = 0; c < new_size; ++c) {
+                    new_data[r].push_back(data[r][name_index[new_columns[c]]]);
+                }
+            }
+
+            DataSet subset;
+            subset.load(new_data, new_columns);
+            subset.data_size = new_data.size();
+
+            return subset;
+        }
+
+	// return subset of original data filtered by conditions
+	DataSet filter(vector<vector<string>> conditions) {
+		if (conditions[0].size() > 2) {
+			cout << "Please format in two columns: {\"Column Name\", condition}" << endl;
+			exit(0);
+		}
+
+		// create map of indices from original column names to positions
+		map<string, int> name_index;
+		for (int i = 0; i < columns.size(); ++i) {
+			name_index.insert(pair<string,int>(columns[i], i));
+		}
+
+		// iterate over conditions and append integer flag for logical type (>, <, >=, etc.)
+		// start with longer ones first
+		string temp_str = "";
+		vector<vector<string>> new_data;
+		vector<vector<bool>> boolean_vector;
+
+		for (int cnd = 0; cnd < conditions.size(); ++cnd) {
+			temp_str = conditions[cnd][1];
+			if (temp_str.substr(0,2) == ">=") { conditions[cnd].push_back("0"); }
+			else if (temp_str.substr(0,2) == "<=") { conditions[cnd].push_back("1"); }
+			else if (temp_str.substr(0,2) == "!=") { conditions[cnd].push_back("2"); }
+			else if (temp_str.substr(0,1) == ">") { conditions[cnd].push_back("3"); }
+			else if (temp_str.substr(0,1) == "<") { conditions[cnd].push_back("4"); }
+			else if (temp_str.substr(0,1) == "=") { conditions[cnd].push_back("5"); }
+			else if (temp_str.substr(0,4) == "[bw]") { conditions[cnd].push_back("6"); } // begins with
+			else if (temp_str.substr(0,4) == "[ew]") { conditions[cnd].push_back("7"); } // ends with
+			else if (temp_str.substr(0,4) == "[ct]") { conditions[cnd].push_back("8"); } // contains
+		}
+			// apply each condition once at a time
+			int current_column;
+			string parsed_value = "";
+
+			for (int cnd = 0; cnd < conditions.size(); ++cnd) {
+				current_column = name_index[conditions[cnd][0]];
+				for (int r = 0; r < this->data_size; ++r) {
+					boolean_vector.push_back(vector<bool>());
+					// check which logical is used (use above map)
+						if(stoi(conditions[cnd][2]) == 0) // >=
+							{
+  							parsed_value = conditions[cnd][1].substr(2,conditions[cnd][1].length());
+  							if (stoi(data[r][current_column]) >= stoi(parsed_value)) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 1) // <=
+  						{
+  							parsed_value = conditions[cnd][1].substr(2,conditions[cnd][1].length());
+  							if (stoi(data[r][current_column]) <= stoi(parsed_value)) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 2) // !=
+							{
+  							parsed_value = conditions[cnd][1].substr(2,conditions[cnd][1].length());
+  							if (data[r][current_column] != parsed_value) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 3) // >
+							{
+  							parsed_value = conditions[cnd][1].substr(1,conditions[cnd][1].length());
+  							if (stoi(data[r][current_column]) > stoi(parsed_value)) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 4) // <
+							{
+  							parsed_value = conditions[cnd][1].substr(1,conditions[cnd][1].length());
+  							if (stoi(data[r][current_column]) < stoi(parsed_value)) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 5) // =
+							{
+  							parsed_value = conditions[cnd][1].substr(1,conditions[cnd][1].length());
+  							if (data[r][current_column] == parsed_value) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 6) // [bw], begins with
+							{
+  							parsed_value = conditions[cnd][1].substr(4,conditions[cnd][1].length());
+  							if (data[r][current_column].substr(0,parsed_value.length()) == parsed_value) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 7) // [ew], ends with
+							{
+  							parsed_value = conditions[cnd][1].substr(4,conditions[cnd][1].length());
+  							int parsed_value_length = parsed_value.length();
+  							int main_length = data[r][current_column].length();
+  							if (data[r][current_column].substr(main_length - parsed_value_length, parsed_value_length) == parsed_value) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+						if(stoi(conditions[cnd][2]) == 8) // [ct], contains
+							{
+  							parsed_value = conditions[cnd][1].substr(4,conditions[cnd][1].length());
+
+  							if (data[r][current_column].find(parsed_value) != string::npos) {
+  								boolean_vector[r].push_back(true);
+  							} else {
+  								boolean_vector[r].push_back(false);
+  							}
+							}
+					}
+			}
+
+		int temp_sum;
+		for (int r = 0; r < this->data_size; ++r) {
+			// sum of row-wise booleans equals column length (i.e, all conditions are true)
+			// then add row to new dataset
+			temp_sum = 0;
+			for (int c = 0; c < boolean_vector[0].size(); ++c) {
+				temp_sum += boolean_vector[r][c];
+			}
+
+			if (temp_sum == boolean_vector[0].size()) {
+				new_data.push_back(data[r]);
+			}
+		}
+
+	    DataSet subset;
+            subset.load(new_data, columns);
+            subset.data_size = new_data.size();
+
+	    return subset;
+	}
+};
+#endif
---
+++ models/classification/DecisionTree.h
@@ -0,0 +1,283 @@
+#ifndef DECISIONTREE_H
+#define DECISIONTREE_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <stdexcept>
+
+class Node {
+	public:
+		Node *left = NULL;
+		Node *right = NULL;
+		std::vector<std::vector<double>> data;
+		std::vector<int> labels;
+		int feature_index;
+		double split_point;
+
+		// majority vote prediction
+		int predict_label() {
+			std::vector<int>::iterator result;
+			result = std::max_element(labels.begin(), labels.end());
+			int result_index = std::distance(labels.begin(), result);
+
+			return labels[result_index];	
+		}
+
+		std::vector<int> get_unique_labels() {
+			std::vector<int> labels_copy = labels;
+			std::vector<int>::iterator ip;
+			ip = std::unique(labels_copy.begin(), labels_copy.end());
+			labels_copy.resize(std::distance(labels_copy.begin(), ip));
+
+			return labels_copy;
+
+		}
+
+		void print_labels() {
+			for (int i = 0; i < labels.size(); ++i) {
+				std::cout << labels[i] << " ";
+			}
+		}
+};
+
+class DecisionTree {
+	private:
+		
+		std::vector<int> get_unique_labels(std::vector<int> labels) {
+			std::vector<int>::iterator ip;
+			ip = std::unique(labels.begin(), labels.end());
+			labels.resize(std::distance(labels.begin(), ip));
+
+			return labels;
+
+		}
+
+		double get_class_proportion(int target_class, std::vector<int> class_labels) {
+			double sum = 0;
+			for (int i = 0; i < class_labels.size(); ++i) {
+				if (class_labels[i] == target_class) {
+					sum += 1;
+				}
+			}
+
+			return sum / class_labels.size();
+		}
+
+		double entropy(std::vector<int> class_labels) {
+			// keep original class labels
+			std::vector<int> original_class_labels = class_labels;
+
+			// get unique class labels
+			std::vector<int>::iterator ip;
+			std::sort(class_labels.begin(), class_labels.end());
+			ip = std::unique(class_labels.begin(), class_labels.end());
+			class_labels.resize(std::distance(class_labels.begin(), ip));
+			
+			// iterate over unique classes, calculate proportion and finally entropy
+			double entropy_sum = 0;
+			double proportion;
+			for (int i = 0; i < class_labels.size(); ++i) {
+				proportion = get_class_proportion(class_labels[i], original_class_labels);
+				entropy_sum -= proportion * log2(proportion);
+			}
+
+			return entropy_sum;
+		}
+
+		double column_median(int column_index, std::vector<std::vector<double>> data) {
+			// tranpose column into row
+			std::vector<double> row_data;
+			row_data.resize(data.size());
+
+			for (int i = 0; i < data.size(); ++i) {
+				row_data[i] = data[i][column_index];
+			}
+
+			// sort and compute median
+			double median;
+			double mid_point = floor(row_data.size() / 2);
+			std::sort(row_data.begin(), row_data.end());
+			if (row_data.size() % 2 == 0) {
+				median = 0.5*(row_data[mid_point] + row_data[mid_point - 1]);
+			} else {
+				median = row_data[mid_point];
+			}
+
+			return median;
+
+		}
+
+		std::vector<double> split_feature(int feature_index, std::vector<std::vector<double>> data, std::vector<int> labels, char feature_type = 'n')  {
+			std::vector<int> left_labels;
+			std::vector<int> right_labels;
+			double split_point;
+			// feature_type = n is numeric
+			// feature type = c is categorical
+			if (feature_type == 'n') {
+				// numeric features will split on median
+				split_point = column_median(feature_index, data);
+				for (int i = 0; i < data.size(); ++i) {
+					if (data[i][feature_index] <= split_point) {
+						left_labels.push_back(labels[i]);
+					} else {
+						right_labels.push_back(labels[i]);
+					}
+				}
+			}
+			
+			double split_entropy = ((double)left_labels.size() / (double)data.size())*entropy(left_labels) + ((double)right_labels.size() / (double)data.size())*entropy(right_labels);
+			std::vector<double> split_feature_data;
+			split_feature_data.push_back(split_entropy);
+			split_feature_data.push_back(split_point);
+
+			// return data about the feature split such as the entropy and split point
+			return split_feature_data;
+		}
+
+		void grow_tree(Node *tree_node, std::vector<std::vector<double>> data, std::vector<int> labels, int max_depth, int current_depth, int min_samples_split) {
+			if (current_depth > max_depth) {
+				throw std::runtime_error("Current tree depth is greater than maximum allowed depth!");
+			}
+
+			double root_entropy = entropy(labels);
+			
+			int feature_to_split = -1;
+			double max_info_gain = -1.0;
+			// iterate over every feature and grow the tree
+			double info_gain;
+			double split_point;
+			std::vector<double> feature_split;
+			for (int i = 0; i < data[0].size(); ++i) {
+				feature_split = split_feature(i, data, labels);
+				info_gain = root_entropy - feature_split[0]; // zeroth index contains the entropy from the split
+				if (info_gain > max_info_gain) {
+					max_info_gain = info_gain;
+					feature_to_split = i;
+					split_point = feature_split[1]; // first index contains the split point of the feature
+				}	
+			}
+
+			// if we gain no further information, break out of recursion by setting current depth greater than max_depth
+			if (max_info_gain == 0) {
+				current_depth = max_depth + 1;
+			}
+
+			std::vector<std::vector<double>> left_data;
+			std::vector<int> left_labels;
+
+			std::vector<std::vector<double>> right_data;
+			std::vector<int> right_labels;
+
+			for (int i = 0; i < data.size(); ++i) {
+				if (data[i][feature_to_split] <= split_point) {
+					left_data.push_back(data[i]);
+					left_labels.push_back(labels[i]);
+				} else {
+					right_data.push_back(data[i]);
+					right_labels.push_back(labels[i]);	
+				}
+			}
+
+			tree_node->split_point = split_point;
+			tree_node->feature_index = feature_to_split;
+
+			tree_node->left = new Node();
+			tree_node->left->data = left_data;
+			tree_node->left->labels = left_labels;
+
+			tree_node->right = new Node();
+			tree_node->right->data = right_data;
+			tree_node->right->labels = right_labels;
+
+			std::vector<int> left_unique = get_unique_labels(left_labels);
+			std::vector<int> right_unique = get_unique_labels(right_labels);
+
+			if (current_depth < max_depth) {
+				// grow tree in both directions
+				// must have at least two samples to split (could be controlled by parameter)
+				// must have more than one unique label (otherwise it's a perfect leaf node)
+				if (tree_node->left->data.size() > (min_samples_split - 1) && left_unique.size() > 1) {
+					grow_tree(tree_node->left, left_data, left_labels, max_depth, current_depth + 1, min_samples_split);
+				} 
+				
+				if (tree_node->right->data.size() > (min_samples_split - 1) && right_unique.size() > 1) {
+					grow_tree(tree_node->right, right_data, right_labels, max_depth, current_depth + 1, min_samples_split);
+			
+				}
+			}
+
+		}
+
+		// traversing tree when calling predict()
+		int predict_down(Node *tree_node, std::vector<double> data) {
+
+			if (tree_node->left == NULL || tree_node->right == NULL) { // leaf
+				return tree_node->predict_label();
+			} else if (data[tree_node->feature_index] <= tree_node->split_point) {
+				return predict_down(tree_node->left, data);
+			} else if (data[tree_node->feature_index] > tree_node->split_point) {
+				return predict_down(tree_node->right, data);
+			}
+
+			return -1;
+		}
+
+	public:
+		Node *root; 
+
+		int get_tree_depth(Node *root) {
+			if (root == NULL) { return 0; }
+			else {
+				int left_height = get_tree_depth(root->left);
+				int right_height = get_tree_depth(root->right);
+				if (left_height >= right_height) {
+					return left_height + 1;
+				} else {
+					return right_height + 1;
+				}
+			}
+		}
+
+		void fit(std::vector<std::vector<double>> data, std::vector<int> labels, int max_depth = 100, int min_samples_split = 2) {
+			if (min_samples_split < 2) {
+				throw std::invalid_argument("Please set min_samples_split parameter to at least 2.");
+			}
+			root = new Node();
+			grow_tree(root, data, labels, max_depth, 1, min_samples_split);
+		}
+
+		std::vector<int> predict(std::vector<std::vector<double>> data) {
+			std::vector<int> predictions;
+			
+			// start at root node and traverse
+			for (int i = 0; i < data.size(); ++i) {
+				predictions.push_back(predict_down(root, data[i]));	
+			}
+
+			return predictions;
+
+		}
+
+		void print_predictions(std::vector<std::vector<double>> data, char style = 'h') {
+			// h = horizontal
+			// v = vertical
+			std::vector<int> predictions = predict(data);
+
+			for (int i = 0; i < predictions.size(); ++i) {
+				if (style == 'h') {
+					std::cout << predictions[i] << " ";	
+				} else if (style == 'v') {
+					std::cout << predictions[i] << std::endl;
+				} else {
+					std::invalid_argument("'style' argument in print_predictions() only accepts 'h' or 'v' as valid inputs.");
+				}
+
+			}
+			std::cout << std::endl;
+		}
+
+};
+
+#endif
