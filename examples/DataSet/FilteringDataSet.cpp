200d1a5fd049480b67cb9cd25b6e474a53c117ef
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2021-09-18 14:39:11 -0400

    Adding lambda filter example

--- examples/DataSet/FilteringDataSet.cpp
+++ examples/DataSet/FilteringDataSet.cpp
@@ -10,10 +10,21 @@ bool my_filter(std::vector<double> indices)
 
 int main()
 {
-    DataSet<double> mydata("example_data.csv");
+    DataSet<double> mydata("datasets/small_regression_test.csv");
 
+    // filtering using external user-defined function (useful if you use the filter multiple times)
     DataSet<double> filtered_data = mydata.filter(&my_filter);
     filtered_data.head();
+
+    // filtering using lambdas (useful if you only intend to use this filter once)
+    // in this case, I'm just replicating the same filter as the external function
+    
+    // note that the std::vector argument must be the same data type as the data set
+    // (in this case, double)
+    DataSet<double> lambda_filter = mydata.filter([](std::vector<double> indices){
+        return (indices[0] > 3.0) && (indices[4] > 5.0);
+    });
+    lambda_filter.head();
 
     return 0;
 }
